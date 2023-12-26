c4d06414e29e78e1d05332034e35662063cc28b1
Fabian <fabiguzmang1205@gmail.com>
2023-12-26 14:22:51 -0600

    Router is implented

--- ProShop/frontend/src/components/Header.jsx
+++ ProShop/frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
@@ -9,10 +10,12 @@ const Header = () => {
         expand="md"/*Size to display hamburguer menu*/ collapseOnSelect>
         <Container>
           {/*Home page*/}
-          <Navbar.Brand href ="/">
-            <img src ={logo} alt ="ProShop"/>
-            ProShop
+          <LinkContainer to ="/">
+            <Navbar.Brand>
+              <img src ={logo} alt ="ProShop"/>
+              ProShop
             </Navbar.Brand>
+          </LinkContainer>
           {/*Place the hamburguer menu*/}
           <Navbar.Toggle aria-controls ="basic-navbar-nav" />
           {/*
@@ -22,8 +25,12 @@ const Header = () => {
           <Navbar.Collapse id ="basic-navbar-nav">
             {/*Navigation Links*/}
             <Nav className='ms-auto'>
-              <Nav.Link href="/cart"><FaShoppingCart/>{/*Icon*/} Cart</Nav.Link>
-              <Nav.Link href="/login"><FaUser/>{/*Icon*/} Sign In</Nav.Link>
+            <LinkContainer to ="/cart">
+              <Nav.Link><FaShoppingCart/>{/*Icon*/} Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to ="/login">
+              <Nav.Link><FaUser/>{/*Icon*/} Sign In</Nav.Link>
+            </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
