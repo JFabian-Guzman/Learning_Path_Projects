77935ea7ea241bdd57c419c0260ff5ef56037a05
Fabian <fabiguzmang1205@gmail.com>
2023-12-26 22:20:40 -0600

    Backend is started

---
+++ ProShop/backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import products from './data/products.js'
+
+const port = 5000/*Local host*/;
+// Initialize express
+const app = express();
+// First route
+app.get( '/' , (req,res) => {
+  // Send text to the route
+  res.send('API is runnig...');
+});
+
+app.get('/api/products' /*Path*/, (req,res) => {
+  // Send a json response with the products
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find( (p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
