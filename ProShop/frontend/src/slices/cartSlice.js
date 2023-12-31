ceea291fb4d5be613b769854dbc00a81b9d6b19f
Fabian <fabiguzmang1205@gmail.com>
2023-12-30 22:40:12 -0600

    New utilities file for the cart was created

--- ProShop/frontend/src/slices/cartSlice.js
+++ ProShop/frontend/src/slices/cartSlice.js
@@ -1,11 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils.js";
 
 const initialState = localStorage.getItem("cart") ? JSON.parse
 (localStorage.getItem("cart")) : {cartItems: []/*Initial state*/};
 
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-}
+
 
 const cartSlice = createSlice({
   name: "cart",
@@ -24,27 +23,10 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
 
-      // Calculate items price
-      state.itemsPrice = addDecimals(state.cartItems.reduce(
-        (acc/*Accumulator*/, item) => acc + item.price * item.qty
-        , 0/*Default for acc*/));
-// Calculate shipping price (If order is over $100 then free, else $10 shipping)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      // Calculate tax price
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2)));
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
 
 export const { addToCart } = cartSlice.actions;
-
 export default cartSlice.reducer;
