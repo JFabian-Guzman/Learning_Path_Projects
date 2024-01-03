9bbe47dea47cd5a31e04f5f07888242770adf4a6
Fabian <fabiguzmang1205@gmail.com>
2024-01-03 08:42:39 -0600

    User routes

--- ProShop/backend/server.js
+++ ProShop/backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT/*Port in file .env*/ || 5000;/*Local host*/
@@ -18,6 +19,7 @@ app.get( '/' , (req,res) => {
 
 // When /api/products is in use automatically will go to the file productRoutes
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
