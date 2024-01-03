329197f639e2933b3277136b89203c79fc89087c
Fabian <fabiguzmang1205@gmail.com>
2024-01-03 11:16:57 -0600

    updateUserProfile and getUserProfile done

---
+++ ProShop/backend/Utils/generateToke.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET
+    , {expiresIn: '30d'});
+
+  // Set JWT as HTTP-Only cookie
+  res.cookie('jwt' , token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'developent',
+    samrSite: 'strict', /* Prevent attacks*/
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days in ms
+  });
+}
+
+export default generateToken
--- ProShop/backend/models/userModel.js
+++ ProShop/backend/models/userModel.js
@@ -30,6 +30,17 @@ userSchema.methods.matchPassword = async function( enteredPassword ) {
     , this.password/*Password store in the DB from this user*/);
 };
 
+// .pre allows to do something before saved in the DB
+userSchema.pre('save'/*Action*/, async function (next) {
+  // determine whether the password field of the user document has been modified
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User
