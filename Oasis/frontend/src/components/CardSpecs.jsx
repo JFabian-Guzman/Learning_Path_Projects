3d99829b714497c4921c8123af9f14b9e74d9f80
Fabian <fabiguzmang1205@gmail.com>
2024-01-28 12:59:12 -0600

    CardSpecs

--- Oasis/frontend/src/components/CardSpecs.jsx
+++ Oasis/frontend/src/components/CardSpecs.jsx
@@ -1,16 +1,25 @@
 import Card from 'react-bootstrap/Card';
-import beach from '../assets/images/beach_img.jpg';
+import house_sale from '../assets/images/house_sale_img.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShower } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const CardSpecs = () => {
   return (
     <Card>
-      <Card.Img variant="top" src={beach} />
+      <div className='price mt-2 ms-2 p-2'>
+        ₡50,000,000
+      </div>
+      <Card.Img variant="top" src={house_sale} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Ciudad, Provincia</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <FontAwesomeIcon className='ms-1 me-2'icon={faShower} /> 2 
+          <FontAwesomeIcon className='ms-4 me-2' icon={faBed} /> 4
+          <FontAwesomeIcon className='ms-4 me-2' icon={faHouse} /> 192m²
         </Card.Text>
       </Card.Body>
     </Card>
